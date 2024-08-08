f78957445ea75f52d6933c0f2f23e3b9e7cf639d
03kev <kevinmuka03@gmail.com>
2024-08-09 00:45:21 +0200

    chore: update plugin configurations and dependencies
    - Change indent width from 2 to 3 in `.stylua.toml`
    - Update various plugin configurations:
       - `copilot-chat.nvim`: Add custom prompts, key mappings, and
    integration with CMP
       - `copilot.lua`: Update configuration for panel and suggestion
    settings
       - `indent-blankline.nvim`: Update indent and scope settings
       - `nvim-hlslens`: Update configuration for search lens and
    integration with vim-visual-multi
       - `vim-visual-multi`: Update default mappings and integration with
    hlslens
    - Separate FileType autocmds for lua and json in `options.lua`

--- lua/myconf/plugins/telescope-file-browser.lua
+++ lua/myconf/plugins/telescope-file-browser.lua
@@ -1,7 +1,6 @@
 return {
-  "jvgrootveld/telescope-zoxide",
-  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-lua/popup.nvim" },
+   "jvgrootveld/telescope-zoxide",
+   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-lua/popup.nvim" },
 
-  vim.keymap.set("n", "<space>ft", ":Telescope file_browser<CR>", { desc = "File browser" }),
+   vim.keymap.set("n", "<space>ft", ":Telescope file_browser<CR>", { desc = "File browser" }),
 }
-
--- lua/myconf/plugins/vim-maximizer.lua
+++ lua/myconf/plugins/vim-maximizer.lua
@@ -1,8 +1,6 @@
--- Plugin for maximizing and restoring windows in Neovim.
-
 return {
-  "szw/vim-maximizer",
-  keys = {
-    { "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Maximize or minimize a splitted window" },
-  },
+   "szw/vim-maximizer",
+   keys = {
+      { "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Maximize or minimize a splitted window" },
+   },
 }
