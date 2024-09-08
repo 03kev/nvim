da656bb845ccb80da508c1692cfa65ce13b257bb
03kev <kevinmuka03@gmail.com>
2024-09-08 18:47:07 +0200

    update plugins and configurations
    Added custom tabline configuration in `options.lua`, including its
    highlights in `theme.lua`. Removed obsolete code in `options.lua`. Added
    `Cpath` command to copy file path to clipboard. Fixed relative number
    position. CopilotChat plugin: fixed float window variable and added
    autocommand to fix relative numbers position. Appended `vertical` to
    `diffopt`. Added `diffview` plugin with configuration. Added `gitsigns`
    configuration with new options and key mappings. Updated `nvim-tree`
    configuration to hide line numbers. Updated `telescope` configuration to
    include directly there `file-browser` and `zoxide` extensions. Fixed
    ignored folders patterns in `telescope` and `nvim-tree`. Sanitized file
    extension for `winbar` highlight group. Removed `highlights.txt` file.

--- lua/myconf/plugins/telescope-zoxide.lua
+++
@@ -1,6 +0,0 @@
-return {
-   "nvim-telescope/telescope-file-browser.nvim",
-   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-
-   vim.keymap.set("n", "<leader>fz", require("telescope").extensions.zoxide.list, { desc = "Zoxide Paths" }),
-}
